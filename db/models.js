560ba8afe47f61fd5a86e0526c4e69ab39984606
Toby Hoshman <t.hoshman@gmail.com>
2018-05-16 20:11:01 -0700

    added patient and appointments queries

--- db/models.js
+++ db/models.js
@@ -1,14 +1,40 @@
 const db = require('./connect');
 
-const clinician = {
+const clinicians = {
   getById(id) {
     return db.one('SELECT * FROM clinicians WHERE id = ${id}', { id });
   },
   getAll() {
     return db.any('SELECT * FROM clinicians');
   };
-}
+};
+
+const patients = {
+  getById(id) {
+    return db.one('SELECT * FROM patients WHERE id = ${id}', { id });
+  },
+  getAll() {
+    return db.any('SELECT * FROM clinicians');
+  };
+};
+
+const appointments = {
+  getByClinician(clinicianId) {
+    return db.one('SELECT * FROM appointments WHERE clinician_id = ${clinicianId}', { clinicianId });
+  },
+  getByPatient(patientId) {
+    return db.one('SELECT * FROM appointments WHERE pateint_id = ${patientId}', { patientId });
+  },
+  getByClinic(clinic) {
+    return db.one('SELECT * FROM appointments WHERE clinic = ${clinic}', { clinic });
+  },
+  getAll() {
+    return db.one('SELECT * FROM appointments');
+  }
+};
 
 module.exports = {
-  clinician,
+  clinicians,
+  patients,
+  appointments,
 };
