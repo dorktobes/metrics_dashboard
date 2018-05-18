5dddb49cfd7f71d6bf38f4bee61409116377d38f
Toby Hoshman <t.hoshman@gmail.com>
2018-05-17 18:01:29 -0700

    wrote Patients type, handled circular dependency in ClinicianTyp

---
+++ server/schema/types/PatientType.js
@@ -0,0 +1,57 @@
+const graphql = require('graphql');
+
+// const ClinicianType = require('./ClinicianType');
+const AppointmentType = require('./AppointmentType');
+
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLList,
+} = graphql;
+
+const PatientType = new GraphQLObjectType({
+  name: 'PatientType',
+  fields: () => (
+    {
+      id: {
+        type: GraphQLID,
+      },
+      first_name: {
+        type: GraphQLString,
+      },
+      last_name: {
+        type: GraphQLString,
+      },
+      address: {
+        type: GraphQLString,
+      },
+      email: {
+        type: GraphQLString,
+      },
+      referral_channel: {
+        type: GraphQLString,
+      },
+      diagnosis: {
+        type: GraphQLString,
+      },
+      clinician: {
+        type: require('./ClinicianType'),
+        resolve({ primary_clinician }, args, context) {
+          if(primary_clinician) {
+            return context.models.clinicians.getById(primary_clinician);
+          }
+          return null;
+        },
+      },
+      appointments: {
+        type: new GraphQLList(AppointmentType),
+        resolve({ id }, args, context) {
+          return context.models.appointments.getByPatient(id);
+        },
+      },
+    }
+  ),
+});
+
+module.exports = PatientType;
