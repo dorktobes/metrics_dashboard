1b4fff81f25f7b6def4fa231dd21bb1ce8d6c3db
Toby Hoshman <t.hoshman@gmail.com>
2018-05-17 17:17:54 -0700

    wrote ClinicianType

---
+++ server/schema/types/ClinicianType.js
@@ -0,0 +1,53 @@
+const graphql = require('graphql');
+
+const PatientType = require('./PatientType');
+const AppointmentType = require('./AppointmentType');
+
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLList,
+  GraphQLFloat,
+} = graphql;
+
+const ClinicianType = new GraphQLObjectType({
+  name: 'ClinicianType',
+  fields: {
+    id: {
+      type: GraphQLID,
+    },
+    first_name: {
+      type: GraphQLString,
+    },
+    last_name: {
+      type: GraphQLString,
+    },
+    clinic: {
+      type: GraphQLFloat,
+    },
+    target_patients_per_day: {
+      type: GraphQLFloat,
+    },
+    days_working: {
+      type: GraphQLFloat,
+    },
+    specialty_1: {
+      type: GraphQLString,
+    },
+    specialty_2: {
+      type: GraphQLString,
+    },
+    specialty_3: {
+      type: GraphQLString,
+    },
+    appointments: {
+      type: new GraphQLList(AppointmentType),
+      resolve({ id }, args, context) {
+        return context.models.appointments.getByClinician(id);
+      },
+    },
+  },
+});
+
+module.exports = ClinicianType;
