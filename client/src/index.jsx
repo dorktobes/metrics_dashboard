49e9817594fed3a33c4ff8fbc01d1e47fe4f5cef
Toby Hoshman <t.hoshman@gmail.com>
2018-05-21 11:32:24 -0700

    Apollo init

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+const link = createHttpLink({
+  uri: '/graphql',
+  fetchOptions: {
+    credentials: 'same-origin',
+  },
+});
+
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id,
+  link,
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.querySelector('#app'),
+);
